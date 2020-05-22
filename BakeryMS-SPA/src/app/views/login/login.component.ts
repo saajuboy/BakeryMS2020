8d9b4496f714214b9a43b66d5c2c3861a7c5c195
Saajidh Umar <saajidhumar@gmail.com>
2020-05-23 04:06:55 +0530

    Implemented login functionality

--- BakeryMS-SPA/src/app/views/login/login.component.ts
+++ BakeryMS-SPA/src/app/views/login/login.component.ts
@@ -1,16 +1,55 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../../_services/auth.service';
+import { AlertifyService } from '../../_services/alertify.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-dashboard',
   templateUrl: 'login.component.html'
 })
-export class LoginComponent {
+export class LoginComponent implements OnInit {
+
+  model: any = {};
+
+  constructor(public authService: AuthService, private alertify: AlertifyService, private router: Router) { }
+
+  ngOnInit() {
+  }
+
+  login() {
+    this.authService.login(this.model).subscribe(next => {
+      this.alertify.success('logged in succesfully');
+    }, error => {
+      this.alertify.error(error);
+    }, () => {
+      this.router.navigate(['']);
+    });
+  }
+
+  loggedin() {
+    return this.authService.loggedIn();
+  }
+
+  logout() {
+    localStorage.removeItem('token');
+    this.alertify.message('logged out');
+    this.router.navigate(['/login']);
+  }
 
   styleObject(): Object {
     return {
       background: 'url(assets/img/Background/Login1.jpg)',
       'background-repeat': 'no-repeat',
       'background-size': '1600px 900px'
+      // filter: 'blur(0px)',
+      // '-moz-filter': 'blur(1px)'
     };
   }
+
+  // styleObjectForLoginForm(): object {
+  //   return {
+  //     filter: 'blur(0px)',
+  //     '-webkit-filter': 'blur(0px)'
+  //   };
+  // }
 }
