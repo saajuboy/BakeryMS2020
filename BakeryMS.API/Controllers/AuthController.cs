475df066a91a70e80cb9c3d3b6d7a051a7c90f24
Saajidh Umar <saajidhumar@gmail.com>
2020-06-24 00:10:21 +0530

    Role Based Auth

--- BakeryMS.API/Controllers/AuthController.cs
+++ BakeryMS.API/Controllers/AuthController.cs
@@ -1,14 +1,18 @@
+using System.Collections.Generic;
 using System;
 using System.IdentityModel.Tokens.Jwt;
 using System.Security.Claims;
 using System.Text;
 using System.Threading.Tasks;
 using BakeryMS.API.Common.DTOs;
+using BakeryMS.API.Data;
 using BakeryMS.API.Data.Interfaces;
 using BakeryMS.API.Models.Profile;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.IdentityModel.Tokens;
+using System.Linq;
 
 namespace BakeryMS.API.Controllers
 {
@@ -18,12 +22,15 @@ namespace BakeryMS.API.Controllers
     {
         private readonly IAuthRepository _repository;
         private readonly IConfiguration _config;
-        public AuthController(IAuthRepository repository, IConfiguration configuration)
+        private readonly DataContext _context;
+        public AuthController(IAuthRepository repository, IConfiguration configuration, DataContext context)
         {
+            _context = context;
             _config = configuration;
             _repository = repository;
         }
 
+        [Authorize(Roles = "Admin")]
         [HttpPost("register")]
         public async Task<IActionResult> Register(UserForRegisterDto userForRegisterDto)
         {
@@ -56,11 +63,26 @@ namespace BakeryMS.API.Controllers
             if (userFromRepository == null)
                 return Unauthorized();
 
-            var claims = new[]
+            // DataContext _context = new DataContext();
+            var userRoles = (from user in _context.Users
+                             join roleMapping in _context.UserRolesMappings
+                                 on user.Id equals roleMapping.User.Id
+                             join role in _context.Roles
+                                 on roleMapping.Roles.Id equals role.Id
+                             where user.Username.ToUpper() == userFromRepository.Username.ToUpper()
+                             select role.RoleName).ToArray();
+            var claims = new List<Claim>
             {
                     new Claim(ClaimTypes.NameIdentifier,userFromRepository.Id.ToString()),
-                    new Claim(ClaimTypes.Name,userFromRepository.Username)
+                    new Claim(ClaimTypes.Name,userFromRepository.Username),
+                    // new Claim(ClaimTypes.Role,userRoles)
             };
+            
+            foreach (var item in userRoles)
+            {
+                claims.Add(new Claim(ClaimTypes.Role, item));
+                
+            }
 
             var key = new SymmetricSecurityKey(Encoding.UTF8
                 .GetBytes(_config.GetSection("AppSettings:Token").Value));
