815de4bee59fe0100a92844c57c60653b4983318
Saajidh Umar <saajidhumar@gmail.com>
2020-10-15 15:37:39 +0530

    Edited UserRepo

--- BakeryMS.API/Data/Interfaces/IUserRepository.cs
+++ BakeryMS.API/Data/Interfaces/IUserRepository.cs
@@ -6,6 +6,8 @@ namespace BakeryMS.API.Data.Interfaces
 {
     public interface IUserRepository
     {
+        void Add<T>(T entity) where T : class;
+        void Delete<T>(T entity) where T : class;
         Task<User> GetUser(int id);
         Task<IEnumerable<User>> GetUsers();
     }
--- BakeryMS.API/Data/Repositories/UserRepository.cs
+++ BakeryMS.API/Data/Repositories/UserRepository.cs
@@ -15,6 +15,20 @@ namespace BakeryMS.API.Data.Repositories
             _context = context;
 
         }
+        public void Add<T>(T entity) where T : class
+        {
+            _context.Add(entity);
+        }
+
+        public void Delete<T>(T entity) where T : class
+        {
+            _context.Remove(entity);
+        }
+
+        public async Task<bool> SaveAll()
+        {
+            return await _context.SaveChangesAsync() > 0;
+        }
         public async Task<User> GetUser(int id)
         {
             return await _context.Users.FirstOrDefaultAsync(a => a.Id == id);
