03dc0d95d15f3905fd70c4ef24bde85782630b25
Saajidh Umar <saajidhumar@gmail.com>
2020-12-21 23:53:43 +0530

    Added DB_Mig For Supp ContactNumber int_To_string

---
+++ BakeryMS.API/Migrations/20201218151326_ChangeSupplierCOntactNumberToString.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace BakeryMS.API.Migrations
+{
+    public partial class ChangeSupplierCOntactNumberToString : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "ContactNumber",
+                table: "Suppliers",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "int",
+                oldNullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "ContactNumber",
+                table: "Suppliers",
+                type: "int",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldNullable: true);
+        }
+    }
+}
--- BakeryMS.API/Models/Inventory/Supplier.cs
+++ BakeryMS.API/Models/Inventory/Supplier.cs
@@ -4,7 +4,7 @@ namespace BakeryMS.API.Models.Inventory
     {
         public int Id { get; set; }
         public string Name { get; set; }
-        public int? ContactNumber { get; set; }
+        public string ContactNumber { get; set; }
         public string Email { get; set; }
         public int? Type { get; set; }
         public string Address { get; set; }
