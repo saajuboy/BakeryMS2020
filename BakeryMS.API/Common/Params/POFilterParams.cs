9722079cf6ebdaa13cdee4f4fdd896c7ccd2448b
Saajidh Umar <saajidhumar@gmail.com>
2020-12-21 23:53:59 +0530

    Added Purchase Order Filter Params

---
+++ BakeryMS.API/Common/Params/POFilterParams.cs
@@ -0,0 +1,8 @@
+namespace BakeryMS.API.Common.Params
+{
+    public class POFilterParams
+    {
+        public int isFromOutlet { get; set; }
+        public bool containNotActive { get; set; }
+    }
+}
