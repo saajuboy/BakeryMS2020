e33694a4e9238ad704176b8a37a818f73f2ef785
Saajidh Umar <saajidhumar@gmail.com>
2020-12-26 06:09:35 +0530

    Updated POH and POD DTOs

--- BakeryMS.API/Common/DTOs/Inventory/PODForDetailDto.cs
+++ BakeryMS.API/Common/DTOs/Inventory/PODForDetailDto.cs
@@ -1,4 +1,5 @@
 using System;
+using System.ComponentModel.DataAnnotations;
 
 namespace BakeryMS.API.Common.DTOs.Inventory
 {
@@ -6,14 +7,23 @@ namespace BakeryMS.API.Common.DTOs.Inventory
     {
         public int Id { get; set; }
         public DateTime DueDate { get; set; }
+        [Required]
+        [Range(0.01, double.MaxValue, ErrorMessage = "Order Quantity should be greater than 0")]
         public decimal OrderQty { get; set; }
         public string Item { get; set; }
+        [Required]
+        [Range(1, int.MaxValue, ErrorMessage = "Item is required")]
         public int ItemId { get; set; }
-        public decimal UnitPrice { get; set; }
-        public decimal LineTotal { get; set; }
-        public decimal ReceivedQuantity { get; set; }
-        public decimal RejectedQuantity { get; set; }
+        public decimal? UnitPrice { get; set; }
+        public decimal? LineTotal { get; set; }
+        public decimal? ReceivedQuantity { get; set; }
+        public decimal? RejectedQuantity { get; set; }
         public decimal? StockedQuantity { get; set; }
         public DateTime? ModifiedTime { get; set; }
+
+        public PODForDetailDto()
+        {
+            this.ModifiedTime = DateTime.Now;
+        }
     }
-} 
+}
--- BakeryMS.API/Common/DTOs/Inventory/POHForDetailDto.cs
+++ BakeryMS.API/Common/DTOs/Inventory/POHForDetailDto.cs
@@ -1,5 +1,7 @@
+using System.Numerics;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 namespace BakeryMS.API.Common.DTOs.Inventory
 {
@@ -7,13 +9,19 @@ namespace BakeryMS.API.Common.DTOs.Inventory
     {
         public int Id { get; set; }
         public int PONumber { get; set; }
+        [Required]
+        [Range(1,int.MaxValue,ErrorMessage="User Required")]
         public int UserId { get; set; }
+        [Required]
+        [Range(1,int.MaxValue,ErrorMessage="Supplier Required")]
         public int SupplierId { get; set; }
-        public bool  Status { get; set; }  
+        public bool Status { get; set; }
         public string DeliveryMethod { get; set; }
-        public DateTime OrderDate { get; set; }
-        public DateTime DeliveryDate { get; set; }
-        public DateTime ModifiedDate { get; set; }
+        [Required]
+        public DateTime? OrderDate { get; set; }
+        [Required]
+        public DateTime? DeliveryDate { get; set; }
+        public DateTime? ModifiedDate { get; set; }
 
         public IList<PODForDetailDto> PODetail { get; set; }
 
