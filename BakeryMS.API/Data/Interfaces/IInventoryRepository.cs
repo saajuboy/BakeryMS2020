79449fd1f6a209f15dca38163338380694039ca2
Saajidh Umar <saajidhumar@gmail.com>
2020-12-26 06:10:55 +0530

    Updated Functions of inventory repo

--- BakeryMS.API/Data/Interfaces/IInventoryRepository.cs
+++ BakeryMS.API/Data/Interfaces/IInventoryRepository.cs
@@ -1,5 +1,6 @@
 using System.Collections.Generic;
 using System.Threading.Tasks;
+using BakeryMS.API.Common.Params;
 using BakeryMS.API.Models.Inventory;
 
 namespace BakeryMS.API.Data.Interfaces
@@ -9,7 +10,9 @@ namespace BakeryMS.API.Data.Interfaces
         void Add<T>(T entity) where T : class;
         void Delete<T>(T entity) where T : class;
         Task<bool> SaveAll();
-        Task<PurchaseOrderHeader> GetPurchaseOrder(int id);
-        Task<IEnumerable<PurchaseOrderHeader>> GetPurchaseOrders();
+        Task<PurchaseOrderHeader> GetPurchaseOrder(int id, POFilterParams filterParams);
+        Task<IEnumerable<PurchaseOrderHeader>> GetPurchaseOrders(POFilterParams filterParams);
+        Task<Supplier> GetSupplier(int id);
+        Task CreatePurchaseOrder(PurchaseOrderHeader purchaseOrderHeader);
     }
 }
--- BakeryMS.API/Data/Repositories/InventoryRepository.cs
+++ BakeryMS.API/Data/Repositories/InventoryRepository.cs
@@ -1,5 +1,7 @@
 using System.Collections.Generic;
+using System.Linq;
 using System.Threading.Tasks;
+using BakeryMS.API.Common.Params;
 using BakeryMS.API.Data.Interfaces;
 using BakeryMS.API.Models.Inventory;
 using Microsoft.EntityFrameworkCore;
@@ -17,7 +19,7 @@ namespace BakeryMS.API.Data.Repositories
 
         public void Add<T>(T entity) where T : class
         {
-            _context.Add(entity);
+            _context.AddRange(entity);
         }
 
         public void Delete<T>(T entity) where T : class
@@ -30,21 +32,85 @@ namespace BakeryMS.API.Data.Repositories
             return await _context.SaveChangesAsync() > 0;
         }
 
-        public async Task<PurchaseOrderHeader> GetPurchaseOrder(int id)
+        public async Task<PurchaseOrderHeader> GetPurchaseOrder(int id, POFilterParams filterParams)
         {
-            var purchaseOrder = await _context.PurchaseOrderHeaders
-            .Include(p => p.PurchaseOrderDetail).ThenInclude(pd => pd.Item)
-            .FirstOrDefaultAsync(a => a.Id == id);
+            var purchaseOrders = _context.PurchaseOrderHeaders.AsQueryable();
+
+            switch (filterParams.isFromOutlet)
+            {
+                case 0:
+                    purchaseOrders = purchaseOrders.Where(a => a.isFromOutlet == true);
+                    break;
+                case 1:
+                    purchaseOrders = purchaseOrders.Where(a => a.isFromOutlet == false);
+                    break;
+                default:
+                    break;
+            }
+
+            if (!filterParams.containNotActive)
+            {
+                purchaseOrders = purchaseOrders.Where(a => a.Status == true);
+            }
+
+
+            var purchaseOrder = await purchaseOrders
+           .Include(p => p.PurchaseOrderDetail).ThenInclude(pd => pd.Item)
+           .FirstOrDefaultAsync(a => a.Id == id);
 
             return purchaseOrder;
-            // throw new System.NotImplementedException();
+
         }
 
-        public async Task<IEnumerable<PurchaseOrderHeader>> GetPurchaseOrders()
+        public async Task<IEnumerable<PurchaseOrderHeader>> GetPurchaseOrders(POFilterParams filterParams)
         {
-            var purchaseOrders = await _context.PurchaseOrderHeaders.ToListAsync();
+            var purchaseOrders = _context.PurchaseOrderHeaders.AsQueryable();
 
-            return purchaseOrders;
+            switch (filterParams.isFromOutlet)
+            {
+                case 0:
+                    purchaseOrders = purchaseOrders.Where(a => a.isFromOutlet == true);
+                    break;
+                case 1:
+                    purchaseOrders = purchaseOrders.Where(a => a.isFromOutlet == false);
+                    break;
+                default:
+                    break;
+            }
+
+            if (!filterParams.containNotActive)
+            {
+                purchaseOrders = purchaseOrders.Where(a => a.Status == true);
+            }
+            return await purchaseOrders.ToListAsync();
         }
+
+        public async Task CreatePurchaseOrder(PurchaseOrderHeader purchaseOrderHeader)
+        {
+            var supID = (int)purchaseOrderHeader.SupplierId;
+            purchaseOrderHeader.Supplier = await GetSupplier(supID);
+
+            var maxId = _context.PurchaseOrderHeaders.Max(model => model.PONumber);
+            purchaseOrderHeader.PONumber = maxId + 1;
+            purchaseOrderHeader.Status = true;
+
+            foreach (var pod in purchaseOrderHeader.PurchaseOrderDetail)
+            {
+                pod.Item = await _context.Items.FirstOrDefaultAsync(a => a.Id == pod.Item.Id);
+            }
+
+            Add(purchaseOrderHeader);
+            // await SaveAll();
+
+        }
+
+        public async Task<Supplier> GetSupplier(int id)
+        {
+            var supplier = await _context.Suppliers.FirstOrDefaultAsync(a => a.Id == id);
+
+            return supplier;
+        }
+
+
     }
 }
