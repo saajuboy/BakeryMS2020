9f946ef110f5681eea4a65d1159e54b16bc2431a
Saajidh Umar <saajidhumar@gmail.com>
2021-01-29 14:57:16 +0530

    changed nav prop for bm

--- BakeryMS-SPA/src/app/_NavObjects/_navBM.ts
+++ BakeryMS-SPA/src/app/_NavObjects/_navBM.ts
@@ -4,36 +4,48 @@ export const navItems: INavData[] = [
     {
         name: 'Dashboard',
         url: '/dashboard',
-        icon: 'icon-speedometer',
-        badge: {
-            variant: 'info',
-            text: 'NEW'
-        }
+        icon: 'icon-speedometer'
+        // badge: {
+        //     variant: 'info',
+        //     text: 'NEW'
+        // }
     },
     {
         title: true,
-        name: 'Theme'
+        name: 'Main Navigation'
     },
+    // {
+    //     name: 'Colors',
+    //     url: '/theme/colors',
+    //     icon: 'icon-drop'
+    // },
+    // {
+    //     name: 'Typography',
+    //     url: '/theme/typography',
+    //     icon: 'icon-pencil'
+    // },
+    // {
+    //     title: true,
+    //     name: 'Components'
+    // },
+    // {
+    //     name: 'Test Module',
+    //     url: '/test',
+    //     icon: 'icon-puzzle',
+    //     roles: ['sdsd', 'dsdsd'],
+    //     attributes: { disabled: false }
+    // },
     {
-        name: 'Colors',
-        url: '/theme/colors',
-        icon: 'icon-drop'
-    },
-    {
-        name: 'Typography',
-        url: '/theme/typography',
-        icon: 'icon-pencil'
-    },
-    {
-        title: true,
-        name: 'Components'
-    },
-    {
-        name: 'Test Module',
-        url: '/test',
-        icon: 'icon-puzzle',
-        roles: ['sdsd', 'dsdsd'],
-        attributes: { disabled: false }
+        name: 'POS',
+        url: '/inventory',
+        icon: 'icon-wallet',
+        children: [
+            {
+                name: 'Purchase Order',
+                url: '/inventory/purchaseOrder',
+                icon: 'icon-list'
+            }
+        ]
     },
     {
         name: 'Inventory',
@@ -53,47 +65,131 @@ export const navItems: INavData[] = [
         ]
     },
     {
-        divider: true
-    },
-    {
-        title: true,
-        name: 'Extras',
-    },
-    {
-        name: 'Pages',
-        url: '/pages',
-        icon: 'icon-star',
+        name: 'Manufacturing',
+        url: '/inventory',
+        icon: 'icon-fire ',
         children: [
             {
-                name: 'Login',
-                url: '/login',
-                icon: 'icon-star'
-            },
-            {
-                name: 'Register',
-                url: '/register',
-                icon: 'icon-star'
-            },
-            {
-                name: 'Error 404',
-                url: '/404',
-                icon: 'icon-star'
-            },
-            {
-                name: 'Error 500',
-                url: '/500',
-                icon: 'icon-star'
+                name: 'Purchase Order',
+                url: '/inventory/purchaseOrder',
+                icon: 'icon-fire '
             }
         ]
     },
     {
-        name: 'Disabled',
-        url: '/dashboard',
-        icon: 'icon-ban',
-        badge: {
-            variant: 'secondary',
-            text: 'NEW'
-        },
-        attributes: { disabled: true },
-    }
+        name: 'Employee Management',
+        url: '/inventory',
+        icon: 'icon-people ',
+        children: [
+            {
+                name: 'Purchase Order',
+                url: '/inventory/purchaseOrder',
+                icon: 'icon-fire '
+            }
+        ]
+    },
+    {
+        name: 'Schedule Routine',
+        url: '/inventory',
+        icon: 'icon-refresh ',
+        children: [
+            {
+                name: 'Purchase Order',
+                url: '/inventory/purchaseOrder',
+                icon: 'icon-fire '
+            }
+        ]
+    },
+    {
+        name: 'Quality Control',
+        url: '/inventory',
+        icon: 'icon-heart ',
+        children: [
+            {
+                name: 'Purchase Order',
+                url: '/inventory/purchaseOrder',
+                icon: 'icon-fire '
+            }
+        ]
+    },
+    {
+        name: 'Reports',
+        url: '/inventory',
+        icon: 'icon-docs ',
+        children: [
+            {
+                name: 'Purchase Order',
+                url: '/inventory/purchaseOrder',
+                icon: 'icon-fire '
+            }
+        ]
+    },
+    {
+        name: 'Master Data',
+        url: '/inventory',
+        icon: 'icon-globe ',
+        children: [
+            {
+                name: 'Purchase Order',
+                url: '/inventory/purchaseOrder',
+                icon: 'icon-fire '
+            }
+        ]
+    },
+    {
+        divider: true
+    },
+    {
+        title: true,
+        name: 'System',
+    },
+    {
+        name: 'Configuration',
+        url: '/inventory',
+        icon: 'icon-settings ',
+        children: [
+            {
+                name: 'Purchase Order',
+                url: '/inventory/purchaseOrder',
+                icon: 'icon-fire '
+            }
+        ]
+    },
+    // {
+    //     name: 'Pages',
+    //     url: '/pages',
+    //     icon: 'icon-star',
+    //     children: [
+    //         {
+    //             name: 'Login',
+    //             url: '/login',
+    //             icon: 'icon-star'
+    //         },
+    //         {
+    //             name: 'Register',
+    //             url: '/register',
+    //             icon: 'icon-star'
+    //         },
+    //         {
+    //             name: 'Error 404',
+    //             url: '/404',
+    //             icon: 'icon-star'
+    //         },
+    //         {
+    //             name: 'Error 500',
+    //             url: '/500',
+    //             icon: 'icon-star'
+    //         }
+    //     ]
+    // },
+    // {
+    //     name: 'Disabled',
+    //     url: '/dashboard',
+    //     icon: 'icon-ban',
+    //     badge: {
+    //         variant: 'secondary',
+    //         text: 'NEW'
+    //     },
+    //     attributes: { disabled: true },
+    // }
 ];
