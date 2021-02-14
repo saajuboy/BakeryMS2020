46f0ab0460fce8519af1cee5798e0f9c9572dc91
Saajidh Umar <saajidhumar@gmail.com>
2021-02-14 09:40:04 +0530

    updated sup and itemsController

--- BakeryMS.API/Controllers/Master/ItemsController.cs
+++ BakeryMS.API/Controllers/Master/ItemsController.cs
@@ -46,16 +46,67 @@ namespace BakeryMS.API.Controllers.Master
         public async Task<IActionResult> CreateItem(ItemForDetailDto itemForDetailDto)
         {
             var itemToCreate = _mapper.Map<Item>(itemForDetailDto);
-            itemToCreate.ItemCategory = _invRepo.Get<ItemCategory>(itemForDetailDto.ItemCategory.Id);
-            itemToCreate.Unit = _invRepo.Get<Unit>(itemForDetailDto.Unit.Id);
+            itemToCreate.ItemCategory = await _invRepo.Get<ItemCategory>(itemForDetailDto.ItemCategory.Id);
+            itemToCreate.Unit = await _invRepo.Get<Unit>(itemForDetailDto.Unit.Id);
 
             _invRepo.Add<Item>(itemToCreate);
 
             if (await _invRepo.SaveAll())
                 return Ok();
 
-            throw new System.Exception($"Failed to Create user on save");
- 
+            throw new System.Exception($"Failed to Create item on save");
+
+        }
+
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> UpdateItem(int id, ItemForDetailDto itemForDetailDto)
+        {
+
+            if (itemForDetailDto.Name == "" ||
+                itemForDetailDto.Name == null ||
+                itemForDetailDto.Code == null ||
+                itemForDetailDto.Code == "" ||
+                itemForDetailDto.ItemCategory == null ||
+                itemForDetailDto.ItemCategory.Id == 0 ||
+                itemForDetailDto.Unit == null ||
+                itemForDetailDto.Unit.Id == 0)
+            {
+                return BadRequest("Insufficent data");
+
+            }
+
+            var itemFromRepository = await _invRepo.GetItem(id);
+            if (itemFromRepository == null)
+                return BadRequest("Item not available");
+
+            var catFromRepo = await _invRepo.Get<ItemCategory>(itemForDetailDto.ItemCategory.Id);
+            var unitFromRepo = await _invRepo.Get<Unit>(itemForDetailDto.Unit.Id);
+
+            itemFromRepository.Name = itemForDetailDto.Name;
+            itemFromRepository.Code = itemForDetailDto.Code;
+            itemFromRepository.Description = itemForDetailDto.Description;
+            itemFromRepository.ItemCategory = catFromRepo;
+            itemFromRepository.Unit = unitFromRepo;
+            itemFromRepository.Type = itemForDetailDto.Type;
+
+
+            if (await _invRepo.SaveAll())
+                return NoContent();
+
+            throw new System.Exception($"Updating item {id} failed on save");
+        }
+
+        [HttpDelete("{id}")]
+        [Authorize(Roles = "Admin,OutletManager,BakeryManager,Cashier")]
+        public async Task<IActionResult> DeleteItem(int id)
+        {
+            var item = await _invRepo.GetItem(id);
+            item.IsDeleted = true;
+            if (await _invRepo.SaveAll())
+                return Ok();
+
+            throw new System.Exception($"Failed to delete item {id}");
         }
     }
 }
--- BakeryMS.API/Controllers/Master/SuppliersController.cs
+++ BakeryMS.API/Controllers/Master/SuppliersController.cs
@@ -2,9 +2,12 @@ using System.Collections.Generic;
 using System.Threading.Tasks;
 using AutoMapper;
 using BakeryMS.API.Common.DTOs.Master;
+using BakeryMS.API.Data;
 using BakeryMS.API.Data.Interfaces;
+using BakeryMS.API.Models.Inventory;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace BakeryMS.API.Controllers.Master
 {
@@ -15,8 +18,10 @@ namespace BakeryMS.API.Controllers.Master
     {
         private readonly IInventoryRepository _invRepo;
         private readonly IMapper _mapper;
-        public SuppliersController(IInventoryRepository invrepo, IMapper mapper)
+        private readonly DataContext _context;
+        public SuppliersController(IInventoryRepository invrepo, IMapper mapper, DataContext context)
         {
+            _context = context;
             _mapper = mapper;
             _invRepo = invrepo;
 
@@ -38,6 +43,58 @@ namespace BakeryMS.API.Controllers.Master
             var supsToReturn = _mapper.Map<IEnumerable<SupplierDto>>(supsFromRepo);
 
             return Ok(supsToReturn);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateSupplier(SupplierDto supplierDto)
+        {
+            var supTOCreate = _mapper.Map<Supplier>(supplierDto);
+            if (await _context.Suppliers.AnyAsync(a => a.Name == supTOCreate.Name))
+                return BadRequest("Supplier already exist");
+
+            _invRepo.Add<Supplier>(supTOCreate);
+
+            if (await _invRepo.SaveAll())
+                return Ok();
+
+            throw new System.Exception($"Failed to Create Supplier on save");
+
+        }
+
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> UpdateSupplier(int id, SupplierDto supplierDto)
+        {
+
+            var supFromRepository = await _invRepo.GetSupplier(id);
+            if (supFromRepository == null)
+                return BadRequest("supplier not available");
+
+             if (await _context.Suppliers.AnyAsync(a => a.Name == supplierDto.Name && a.Id != supplierDto.Id))
+                return BadRequest("Supplier already exist");
+
+            supFromRepository.Name = supplierDto.Name;
+            supFromRepository.ContactNumber = supplierDto.ContactNumber;
+            supFromRepository.Email = supplierDto.Email;
+            supFromRepository.Address = supplierDto.Address;
+            supFromRepository.Type = supplierDto.Type;
+
+            if (await _invRepo.SaveAll())
+                return NoContent();
+
+            throw new System.Exception($"Updating supplier {id} failed on save");
+        }
+
+        [HttpDelete("{id}")]
+        [Authorize(Roles = "Admin,OutletManager,BakeryManager,Cashier")]
+        public async Task<IActionResult> DeleteSupplier(int id)
+        {
+            var supplier = await _invRepo.GetSupplier(id);
+            supplier.IsDeleted = true;
+            if (await _invRepo.SaveAll())
+                return Ok();
+
+            throw new System.Exception($"Failed to delete supplier {id}");
         }
     }
 }
