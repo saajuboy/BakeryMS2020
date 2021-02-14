00620203d080d6a09f7e3ac48deb78823f27e903
Saajidh Umar <saajidhumar@gmail.com>
2021-02-14 09:40:19 +0530

    Updated validations in Dto

--- BakeryMS.API/Common/DTOs/Master/ItemCategoryForDetailDto.cs
+++ BakeryMS.API/Common/DTOs/Master/ItemCategoryForDetailDto.cs
@@ -1,9 +1,15 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace BakeryMS.API.Common.DTOs.Master
 {
     public class ItemCategoryForDetailDto
     {
         public int Id { get; set; }
+        [Required(ErrorMessage="Code Required")]
+        [RegularExpression("^[a-zA-Z]{4}",ErrorMessage="Code should be only 4 alpha characters eg:- ABCD")]
+
         public string Code { get; set; }
+        [Required]
         public string Description { get; set; }
     }
 }
--- BakeryMS.API/Common/DTOs/Master/SupplierForListDto.cs
+++ BakeryMS.API/Common/DTOs/Master/SupplierForListDto.cs
@@ -1,11 +1,16 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace BakeryMS.API.Common.DTOs.Master
 {
     public class SupplierDto
     {
         public int Id { get; set; }
+        [Required]
         public string Name { get; set; }
+        [Required]
         public string ContactNumber { get; set; }
         public string Email { get; set; }
+        [Required]
         public int? Type { get; set; }
         public string Address { get; set; }
     }
