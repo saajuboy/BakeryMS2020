c641658856c1ffb66e1df1357152c4ab3c208306
Saajidh Umar <saajidhumar@gmail.com>
2021-02-14 09:37:40 +0530

    added service method for item,cat,unit,sup

--- BakeryMS-SPA/src/app/_services/master.service.ts
+++ BakeryMS-SPA/src/app/_services/master.service.ts
@@ -2,7 +2,7 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { environment } from '../../environments/environment';
-import { Item, ItemForDropdown } from '../_models/item';
+import { Item, ItemCategory, ItemForDropdown, Unit } from '../_models/item';
 import { Supplier, SupplierForDropdown } from '../_models/supplier';
 
 @Injectable({
@@ -20,25 +20,80 @@ export class MasterService {
     } else {
       return this.http.get<Supplier[]>(this.baseUrl + 'suppliers');
     }
-
-
   }
   getSupplier(id): Observable<Supplier> {
-    return this.http.get<Supplier>(this.baseUrl + 'users/' + id);
+    return this.http.get<Supplier>(this.baseUrl + 'suppliers/' + id);
+  }
+  CreateSupplier(supplier: Supplier) {
+    return this.http.post(this.baseUrl + 'suppliers', supplier);
+  }
+  updateSupplier(id: number, supplier: Supplier) {
+    return this.http.put(this.baseUrl + 'suppliers/' + id, supplier);
+  }
+  deleteSupplier(id: number) {
+    return this.http.delete(this.baseUrl + 'suppliers/' + id);
   }
 
   getItems(isForDropdown: boolean): Observable<Item[]> | Observable<ItemForDropdown[]> {
-
     if (isForDropdown === true) {
       return this.http.get<ItemForDropdown[]>(this.baseUrl + 'items');
     } else {
       return this.http.get<Item[]>(this.baseUrl + 'items');
     }
-
-
+  }
+  CreateItem(item: Item) {
+    return this.http.post(this.baseUrl + 'items', item);
   }
   getItem(id): Observable<Item> {
     return this.http.get<Item>(this.baseUrl + 'items/' + id);
+  }
+  updateItem(id: number, item: Item) {
+    return this.http.put(this.baseUrl + 'items/' + id, item);
+  }
+  patchItem(id: number, item: any) {
+    return this.http.patch(this.baseUrl + 'items/' + id, item);
+  }
+  deleteItem(id: number) {
+    return this.http.delete(this.baseUrl + 'items/' + id);
+  }
+
+
+  getItemCode(id: number) {
+    return this.http.get(this.baseUrl + 'itemCategory/getcode/' + id);
+  }
+
+
+  getItemCategories(): Observable<ItemCategory[]> {
+    return this.http.get<ItemCategory[]>(this.baseUrl + 'itemCategory');
+  }
+  getItemCategory(id): Observable<ItemCategory> {
+    return this.http.get<ItemCategory>(this.baseUrl + 'itemCategory/' + id);
+  }
+  CreateItemCategory(itemCategory: ItemCategory) {
+    return this.http.post(this.baseUrl + 'itemCategory', itemCategory);
+  }
+  updateItemCategory(id: number, itemCategory: ItemCategory) {
+    return this.http.put(this.baseUrl + 'itemCategory/' + id, itemCategory);
+  }
+  deleteItemCategory(id: number) {
+    return this.http.delete(this.baseUrl + 'itemCategory/' + id);
+  }
+
+
+  getUnits(): Observable<Unit[]> {
+    return this.http.get<Unit[]>(this.baseUrl + 'units');
+  }
+  getUnit(id): Observable<Unit> {
+    return this.http.get<Unit>(this.baseUrl + 'units/' + id);
+  }
+  CreateUnit(unit: Unit) {
+    return this.http.post(this.baseUrl + 'units', unit);
+  }
+  updateUnit(id: number, unit: Unit) {
+    return this.http.put(this.baseUrl + 'units/' + id, unit);
+  }
+  deleteUnit(id: number) {
+    return this.http.delete(this.baseUrl + 'units/' + id);
   }
 
 }
